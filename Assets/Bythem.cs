7bbc345760232aa1bae88262047ec064392d8cf1
Peter Pavlik <peter.pavlik.14@gmail.com>
2020-11-08 14:14:05 +0100

    Evil Johny reveal

--- Assets/Bythem.cs
+++ Assets/Bythem.cs
@@ -7,6 +7,7 @@ public class Bythem : MonoBehaviour
     private bool moving = false;
     private Vector3 destination;
     public float speed;
+    public SpriteRenderer evil_johny;
 
     // Update is called once per frame
     void Update()
@@ -18,6 +19,7 @@ public class Bythem : MonoBehaviour
             if (Vector3.Distance(transform.position, destination) < 0.01f)
             {
                 moving = false;
+                StartCoroutine(FadeInEvilJohny());
             }
         }
     }
@@ -32,5 +34,18 @@ public class Bythem : MonoBehaviour
     {
         transform.position = new Vector3(10f, 0f, 0f);
         this.moving = false;
+        evil_johny.color = new Color(255, 255, 255, 0f);
+    }
+
+    private IEnumerator FadeInEvilJohny()
+    {
+        int i = 0;
+        int steps = 8;
+        while (evil_johny.color != new Color(255, 255, 255, 1f))
+        {
+            yield return new WaitForSeconds(0.25f);
+            evil_johny.color = new Color(255, 255, 255, 1f * i / steps);
+            i++;
+        }
     }
 }
